807ca93bcf15d7fa9e61436030837fe0ab1b8563
harjeevansingh <harjeevansingh01@gmail.com>
2022-04-23 21:26:54 +0530

    chore: first commit

---
+++ src/main/java/com/imdbLite/imdbLite/ImdbLiteApplication.java
@@ -0,0 +1,13 @@
+package com.imdbLite.imdbLite;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ImdbLiteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ImdbLiteApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/imdbLite/imdbLite/ImdbLiteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.imdbLite.imdbLite;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ImdbLiteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
